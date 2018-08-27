89be5bfb6dce84aaa6c7b6f50e812b92f74b9bd4
Akash <akashmkabra@gmail.com>
2018-08-27 17:28:24 -0400

    Add menu to main activity and add list view into the main activity.

--- app/src/main/java/com/example/android/mealcheck/MainActivity.java
+++ app/src/main/java/com/example/android/mealcheck/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.mealcheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/settings_menu.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            case R.id.settings:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Add Food" menu option
+            case R.id.add_food:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Delete all" menu option
+            case R.id.delete_all:
+                // Do nothing for now
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
