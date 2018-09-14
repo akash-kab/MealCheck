374d4a47d1bedbc078da247c18cdc974bfe5fe3b
Akash <akashmkabra@gmail.com>
2018-09-14 09:05:28 -0400

    updating some info

--- app/src/main/java/com/example/android/mealcheck/AddFoodActivity.java
+++ app/src/main/java/com/example/android/mealcheck/AddFoodActivity.java
@@ -1,22 +1,25 @@
 package com.example.android.mealcheck;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.example.android.mealcheck.dataSQL.DatabaseHelper;
+import com.example.android.mealcheck.dataSQL.Contract.MealEntry;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AddFoodActivity extends AppCompatActivity {
+
+    DatabaseHelper mdHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +35,6 @@ public class AddFoodActivity extends AppCompatActivity {
     EditText enterText;
     List<String> foodNames = new ArrayList<String>();
 
-    //EditText firstEntry = (EditText) findViewById(R.id.firstFood);
 
     int b = 0;
     int k = 0;
@@ -88,6 +90,18 @@ public class AddFoodActivity extends AppCompatActivity {
     }
 
 
+    public void addToDB() {
+        SQLiteDatabase db = mdHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        for (int i = 0; i < foodNames.size(); i++) {
+            values.put(MealEntry.COLUMN_MEAL_NAME, foodNames.get(i));
+        }
+
+        //depending on what activity we came from, we add it to the respectful table.
+        db.insert(MealEntry.TABLE_MONDAY, null, values);
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/addfood_done.xml file.
@@ -101,6 +115,7 @@ public class AddFoodActivity extends AppCompatActivity {
         // User clicked on a menu option in the app bar overflow menu
         switch (item.getItemId()) {
             case R.id.done:
+                addToDB();
                 finish();
                 return true;
         }
--- app/src/main/java/com/example/android/mealcheck/dataSQL/MealProvider.java
+++ app/src/main/java/com/example/android/mealcheck/dataSQL/MealProvider.java
@@ -1,4 +1,47 @@
 package com.example.android.mealcheck.dataSQL;
 
-public class MealProvider {
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class MealProvider extends ContentProvider {
+
+    private DatabaseHelper dbHelper;
+
+    @Override
+    public boolean onCreate() {
+        dbHelper = new DatabaseHelper(getContext());
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
 }
